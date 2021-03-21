af69838f2130d999c8ad7f074b06643f7fbee70a
ahmetburkan <ahmetburkan@outlook.com>
2021-03-21 16:17:20 +0100

    added share

--- app/src/main/java/com/app/softwaredevelopmentpraktijk1/GalleryActivity.java
+++ app/src/main/java/com/app/softwaredevelopmentpraktijk1/GalleryActivity.java
@@ -3,12 +3,17 @@ package com.app.softwaredevelopmentpraktijk1;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Parcelable;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +23,9 @@ import android.widget.LinearLayout;
 import com.app.softwaredevelopmentpraktijk1.Model.StorageModel;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class GalleryActivity extends AppCompatActivity {
 
@@ -28,13 +36,23 @@ public class GalleryActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
+        // TODO: Frits
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+        builder.detectFileUriExposure();
+
         StorageModel storageModel = new StorageModel();
+        Context context = this;
 
         File folder = new File(String.valueOf(storageModel.getStorageDirectory()));
         File[] filesInFolder = folder.listFiles();
 
         for (File file : filesInFolder) {
             if (!file.isDirectory()) {
+                File imageFile = new File(folder, file.getName());
+                Uri imageUri =  Uri.fromFile(imageFile);
+                // TODO: Frits
+//                Uri imageUri = FileProvider.getUriForFile(context, context.getApplicationContext().getPackageName() + ".fileprovider", imageFile);
 
                 // Create wrapper for ImageView
                 final LinearLayout linearLayoutWrapper = new LinearLayout(this);
@@ -52,31 +70,27 @@ public class GalleryActivity extends AppCompatActivity {
                 imageView.setMinimumHeight(300);
 
 
-                final Button mailButton = new Button(this);
-                mailButton.setText("Share");
-
-                Intent intent = new Intent(this, ShareActivity.class);
-
-
-                mailButton.setOnClickListener(new View.OnClickListener() {
+                final Button shareButton = new Button(this);
+                shareButton.setText("Share");
+                shareButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        intent.putExtra("imagepath", imagePath);
-                        startActivity(intent);
+                        Intent shareIntent = new Intent();
+                        shareIntent.setAction(Intent.ACTION_SEND);
+                        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                        shareIntent.setType("image/*");
+                        shareIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
+                        startActivity(Intent.createChooser(shareIntent,"Share"));
                     }
                 });
 
-
                 // Add image to wrapper
                 linearLayoutWrapper.addView(imageView);
-                linearLayoutWrapper.addView(mailButton);
+                linearLayoutWrapper.addView(shareButton);
 
                 // Add wrapper to scrollview
                 LinearLayout linearLayout = findViewById(R.id.scrollview);
                 linearLayout.addView(linearLayoutWrapper);
-
-
-
             }
         }
     }
--- app/src/main/java/com/app/softwaredevelopmentpraktijk1/Model/GalleryModel.java
+++
@@ -1,8 +0,0 @@
-package com.app.softwaredevelopmentpraktijk1.Model;
-
-public class GalleryModel extends StorageModel {
-
-    GalleryModel() {
-
-    }
-}
